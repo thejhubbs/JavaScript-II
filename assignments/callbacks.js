a1e70eebafd7ce65eea8b4dbe5b8d5891509c840
Jordan Hubbard <thejhubbs@gmail.com>
2019-04-20 12:52:14 -0400

    Finished projects

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,50 +1,71 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+//You have been provided an example of a problem and a solution to see how this works with our items array.  
+//Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
 
-  // Potential Solution:
+//Given this problem: 
 
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+// function firstItem(arr, cb) {
+//   // firstItem passes the first item of the given array to the callback function.
+// }
 
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+// Potential Solution:
 
-*/
-
+// Higher order function using "cb" as the call back
+function firstItem(arr, cb) {
+  return cb(arr[0]);
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  var bool = false;
+  list.forEach(function(arr_item) {
+    if(item === arr_item) { bool = true; }
+  });
+  return cb(bool);
 }
+
+
+// Function invocation 
+firstItem(items, function (first) {
+  console.log(first)
+});
+
+getLength(items, function (length) {
+  console.log(length);
+});
+
+last(items, function(last) {
+  console.log(last);
+});
+
+contains('Pencil', items, function(bool){
+  console.log(bool);
+});
+
+contains('Bookbag', items, function(bool){
+  console.log(bool);
+});
 
 /* STRETCH PROBLEM */
 
